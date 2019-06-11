e3ed5c2f00dc2b0b13ec33ca99255156cfaea482
petr0n <petron@gmail.com>
2019-06-11 12:37:32 -0500

    install tailwind - see Google doc for set up instructions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-4xl">FINDO GAMES</h1>
+        <h4 className="text-sm">PRESENTS</h4>
+        <h2 className="text-6xl">FINDO</h2>
+        <p className="text-sm">The game that blends classic BINGO and a live scavenger hunt</p>
       </header>
     </div>
   );
