8f65ebd58c0370b6b91b22d81ba7b932023c259b
petr0n <petron@gmail.com>
2019-06-25 18:26:55 -0500

    debug privacy page

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ if (process.env.NODE_ENV === "production") {
   require('dotenv'); 
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
 
 app.use(routes);
 
