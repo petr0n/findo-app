8217ddd37f25ce77d5209edf200ecaa6e405cdbf
jasonfleming1 <jason.fleming@comcast.net>
2019-06-26 20:19:15 -0500

    adding more components

--- client/src/components/TilesManage/TilesManage.js
+++ client/src/components/TilesManage/TilesManage.js
@@ -2,6 +2,8 @@
 //=======================================================
 import React from "react";
 import "./TilesManage.css";
+import "./btnApprove";
+import "./btnDeny";
 //import { Link } from "react-router-dom"
 
 //CONTENT
@@ -13,3 +15,18 @@ const TilesManage = () => (
 //EXPORT
 //=======================================================
 export default TilesManage;
+
+//REQUIREMENTS
+//=======================================================
+
+/*
+
+TilesManage (page 11, component to be passed to Admin =>)
+Description: read from db a list of all tiles pending admin approval for use
+Imports: React, btnDeny, btnApprove
+Contains Files: 
+btnDeny = PUT that changes the <value> of status (copy, paste status value)
+btnApprove = PUT that changes the <value> of status (copy, paste status value)
+
+
+*/
