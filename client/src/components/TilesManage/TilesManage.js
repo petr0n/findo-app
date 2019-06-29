11da201e6f008c61af29a99b8d2623fab0676be0
Emily <raper003@umn.edu>
2019-06-29 10:36:25 -0500

    TilesManage.js and TilesView.js

--- client/src/components/TilesManage/TilesManage.js
+++ client/src/components/TilesManage/TilesManage.js
@@ -1,3 +1,8 @@
+// What this page does:
+// Displays all pending tiles
+// Give an option for Approve (this creates a new tile and updates the current tiles db, adding a new active tile)
+// Gives an option for deny (this simply removes suggestion from suggestions db)
+
 //IMPORT
 //=======================================================
 import React from "react";
@@ -7,8 +12,33 @@ import "./TilesManage.css";
 //CONTENT
 //=======================================================
 const TilesManage = () => (
-  <div ></div>
-);
+
+  function getPendingTiles () {  // gets all Pending tiles
+    return axios.get("/api/tiles?status=Pending");
+  };
+
+  handleApproveClick = () => {
+    function createTile (req, res) {
+      return axios.post("/api/tiles", tileData);
+      db.Tile
+          .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+  handleDenyClick = () => {
+    function deleteTile (req, res) {
+      return axios.delete("/api/tiles/" + id);
+      .then(response => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
+
 
 //EXPORT
 //=======================================================
--- client/src/components/TilesView/TilesView.js
+++ client/src/components/TilesView/TilesView.js
@@ -1,15 +1,97 @@
+// What this page does:
+// This page displays all tiles
+// Gives an option to Edit a tile (this take you to the tileEdit component)
+// Gives and option to deactivate a tile (this updates the status of the tile)
+// Give an option to activate a tile (this updates the status of the tile) 
+
 //IMPORT
 //=======================================================
-import React from "react";
+import React, {Component} from "react";
 import "./TilesView.css";
-//import { Link } from "react-router-dom"
+import "./Tile.js";
+import "./Buttons/btnActivate";
+import "./Buttons/btnDeactivate";
+import ".Buttons/btnEdit";
 
-//CONTENT
-//=======================================================
-const TilesView = () => (
-  <div ></div>
-);
+const db = require("../models");
 
-//EXPORT
-//=======================================================
-export default TilesView;
+module.exports = {
+
+    // Find all tiles
+    findAllTiles: function (req, res) {
+        db.Tile
+            .find(req.query)
+            .populate("createdBy")
+            .populate("approvedBy")
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+  }
+  
+  // create a list to put the tile names and info in
+	makeTileList = () => {
+		let tiles = db.Tile;
+		for(let i = 0;i < 25; i++){
+			if (status === active) { // if the tile is currently active, render the deactivate button on the list
+				render () 
+          return {
+              btnDeactivate
+          }
+			}
+      else { // if the tile is currently deactive, render the activate button on the list
+          return {
+            btnActivate
+          }
+      }
+		}
+		console.log('tiles', tiles); // return the tiles
+		return tiles;
+	};
+
+	renderList = () => { // render the list
+		this.setState({ 
+			tiles: this.makeTileList() 
+		});
+	};
+
+	componentDidMount() {
+    this.renderList();
+  };
+
+  // Click Handlers
+
+  handleDeactivateClick = () => {
+    function updateTile (req, res) {
+      return axios.put("/api/tiles/" + id, tileData);
+      db.Tile
+          .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+  handleActivateClick = () => {
+    function updateTile (req, res) {
+      return axios.put("/api/tiles/" + id, tileData);
+      .then(response => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+  handleEditClick = () => {
+    render () {
+      return (
+        tileEdit
+      )}
+      }).catch((error) => {
+        console.log(error);
+      };
+window.onload(console.log(db.Tile));
+
+  
+
+
+
