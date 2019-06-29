fed030b6603069c846a21f6e4cc8fedb20236383
onahbosen <onahbosen@gmail.com>
2019-06-29 12:27:45 -0500

    admin nav

--- client/src/components/AdminNav/AdminNav.js
+++ client/src/components/AdminNav/AdminNav.js
@@ -1,46 +1,29 @@
 //IMPORT
 //=======================================================
-import React from "react";
+import React, {Component} from "react";
 import "./AdminNav.css";
-import "./btnAdd";
-import "./btnSuggested";
-import "./btnView";
-import Header from "../../Header";
-import Footer from "../../Footer";
-import AdminWrapper from "../../AdminWrapper";
-import TileAdd "../../TileAdd";
-import TilesManage from "../TilesManage";
-import TilesView from "../TilesView";
+
 
 //CONTENT
 //=======================================================
-class NavBar extends Component {
+class AdminNav extends Component {
     displayTiles = () => {
         this.setState({
             displayTiles: !this.state.displayTiles
         })
     }
-}
 
-render(){
-    return (
-        <AdminWrapper>
-            <Header />
+    render() {
+        return (
             <nav className="admin-nav">
                 <ul>
-                    <li className="suggested" onClick={this.displayTiles}>Suggested</li>
-                    <li className="view-all" onClick={this.displayTiles}>View All</li>
-                    <li className="add-tile" onClick={this.displayTiles}>Add Tile</li>
+                    <li className="suggested" onClick={() => this.props.handleNavClick("suggest")}>Suggested</li>
+                    <li className="view-all" onClick={() => this.props.handleNavClick("view all")}>View All</li>
+                    <li className="add-tile" onClick={() => this.props.handleNavClick("add tile")}>Add Tile</li>
                 </ul>
             </nav>
-            <div>
-                <TilesManage/>
-                <TilesView/>
-                <TileAdd/>
-            </div>
-            <Footer />
-        </AdminWrapper>
-    )
-};
+        )
+    }
+}
 
 export default AdminNav;
