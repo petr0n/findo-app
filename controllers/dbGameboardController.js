62e3c0b15b4de9dc3fc655da624e540e56c8659d
Julia F <46096629+juliafercello@users.noreply.github.com>
2019-06-29 11:51:53 -0500

    stuff

--- controllers/dbGameboardController.js
+++ controllers/dbGameboardController.js
@@ -62,7 +62,7 @@ module.exports = {
                                 .then(dbModel => res.json(dbModel))
                                 .catch(err => res.status(422).json(err));
                         })
-                            .catch(err => res.status(422).json(err));
+                     .catch(err => res.status(422).json(err));
                     }
                 }
             }
