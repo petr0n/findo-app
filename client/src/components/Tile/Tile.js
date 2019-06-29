9bc5443bd4cb9164fa0e1d51ffbe7de0cab0291b
petr0n <petron@gmail.com>
2019-06-28 19:54:19 -0500

    fix tile spacing

--- client/src/components/Tile/Tile.js
+++ client/src/components/Tile/Tile.js
@@ -20,14 +20,12 @@ class Tile extends Component {
   }
     
   render() {
-    console.log('this.props.tileData', this.props.tileData);
+    // console.log('this.props.tileData', this.props.tileData);
     const tileData = this.props.tileData;
     return (
-      <div className="tile-container">
-        <div className="tile-wrapper flex items-center justify-center text-center" onClick={() => this.handleTileClick(this.props.tileId)}>
-          <div className={"fade-in-fwd tile md_p-3 sm_p-1 flex items-center justify-center text-center " + (this.props.isCenter ? 'center-tile' : '')}> 
-            {tileData.tileText}
-          </div>
+      <div className="tile-wrapper flex items-center justify-center text-center p-1" onClick={() => this.handleTileClick(this.props.tileId)}>
+        <div className={"fade-in-fwd tile md_p-3 sm_p-1 flex items-center justify-center text-center rounded " + (this.props.isCenter ? 'center-tile' : '')}> 
+          {tileData.tileText}
         </div>
       </div>
     )
