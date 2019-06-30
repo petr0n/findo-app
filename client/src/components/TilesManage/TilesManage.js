660377d71eb425274f1a83bdaf646086b4a50226
petr0n <petron@gmail.com>
2019-06-30 10:17:54 -0500

    fix missing file issues

--- client/src/components/TilesManage/TilesManage.js
+++ client/src/components/TilesManage/TilesManage.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-// import "./TilesView.css";
+import "./TilesManage.css";
 // import "./Tile.js";
 // import "./Buttons/btnActivate";
 // import "./Buttons/btnDeactivate";
