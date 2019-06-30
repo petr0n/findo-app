f56ca9c6ad9c8a1a714a53e42c7e59764bea0a9c
petr0n <petron@gmail.com>
2019-06-30 10:05:12 -0500

    update for to htmlFor

--- client/src/components/TileAdd/TileAdd.js
+++ client/src/components/TileAdd/TileAdd.js
@@ -48,7 +48,7 @@ class TileAdd extends Component {
       <div className="w-full max-w-xs">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="tileText">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tileText">
             Tile Copy:
             </label>
             <input rows="20" col="5" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-auto"
