c228c42ed7a003685d7dcc64c462961882b610ff
petr0n <petron@gmail.com>
2019-06-30 09:55:29 -0500

    update admin components and pages2

--- client/src/components/AdminNav/AdminNav.js
+++ client/src/components/AdminNav/AdminNav.js
@@ -16,14 +16,14 @@ class AdminNav extends Component {
 		const inactiveItem = "text-center block border-orange-100 border bg-orange-100 rounded hover:border-gray-200 text-orange-500 hover:bg-gray-200 py-2 px-4 cursor-pointer";
 		return (
 			<nav className="mb-8">
-				<ul class="flex">
-					<li class="flex-1 mr-2">
+				<ul className="flex">
+					<li className="flex-1 mr-2">
 						<div className={activeItem} onClick={() => this.props.handleNavClick("tilesmanage")}>Suggested</div>
 					</li>
-					<li class="flex-1 mr-2">
+					<li className="flex-1 mr-2">
 						<div className={inactiveItem} onClick={() => this.props.handleNavClick("tilesview")}>View All</div>
 					</li>
-					<li class="text-center flex-1">
+					<li className="text-center flex-1">
 						<div className={inactiveItem} onClick={() => this.props.handleNavClick("addtile")}>Add Tile</div>
 					</li>
 				</ul>
