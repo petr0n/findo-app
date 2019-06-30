97f2a8205fcdd5dd52eb5276ed27d959f64340de
Emily <raper003@umn.edu>
2019-06-30 12:39:12 -0500

    New components created for TilesView.js and TilesManage.js lists

--- client/src/components/TilesManage/TilesManage.js
+++ client/src/components/TilesManage/TilesManage.js
@@ -1,11 +1,21 @@
+//IMPORT
+//=======================================================
+
 import React, {Component} from "react";
 import "./TilesManage.css";
-// import "./Tile.js";
-// import "./Buttons/btnActivate";
-// import "./Buttons/btnDeactivate";
-// import ".Buttons/btnEdit";
+import "../List"; // componenet to hold the list
+import "../ListItem";  // will hold each item/tile in pending tiles list
 import tileApi from "../../utils/tileAPI";
+import "./Tile.js";
+import "./Buttons/btnActivate";
+import "./Buttons/btnDeactivate";
+import ".Buttons/btnEdit";
 
+// this page will act like a "page" would, where it will render the UL/LI and buttons for pending tiles
+//AdminNav will import this component
+
+//CONTENT
+//=======================================================
 class TilesManage extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +23,9 @@ class TilesManage extends Component {
       tileText: " ",
     }
   }
+
+    // load all pending tiles and save them to the state
+  //====================================================
 
     loadPendingTiles = () => {
       tileApi.getPendingTiles()
@@ -25,6 +38,15 @@ class TilesManage extends Component {
     componentDidMount() {
       this.loadPendingTiles();
     }
+
+      // handleEditTile render the edit tile component
+  //====================================================
+
+  // handleActivate/Deactivate
+  //====================================================
+
+  //render
+  //====================================================
 
   render() {
     // const { tileText } = this.state;
