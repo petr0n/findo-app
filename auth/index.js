7c568fe0c4815e5c889edeee708456707cacf915
petr0n <petron@gmail.com>
2019-06-30 19:32:50 -0500

    auth testing10

--- auth/index.js
+++ auth/index.js
@@ -10,15 +10,15 @@ passport.serializeUser((user, done) => {
 	console.log('=== serialize ... called ===')
 	console.log(user) // the whole raw user object!
 	console.log('---------')
-	done(null, { _id: user._id })
+	done(null, user.id)
 })
 
 passport.deserializeUser((id, done) => {
 	console.log('DEserialize ... called');
-	console.log('_id', id);
+	console.log('userid', id);
 	db.User.findById(id)
 	.then(user => {
-		console.log('======= DESERILAIZE USER CALLED ======')
+		console.log('======= DESERIALIZE USER CALLED ======')
 		console.log(user)
 		console.log('--------------')
 		done(null, user)
