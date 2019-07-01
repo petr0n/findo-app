4ef738e44a0870081f17ece727318cce3859f5e7
petr0n <petron@gmail.com>
2019-06-30 21:37:10 -0500

    auth testing18

--- auth/facebookStrategy.js
+++ auth/facebookStrategy.js
@@ -12,7 +12,7 @@ const strategy = new FacebookStrategy({
 	function(accessToken, refreshToken, profile, cb) {
 		console.log('fb login', profile);
 		let userData = {
-			id: profile.id,
+			_id: profile.id,
 			email: profile.emails[0].value,
 			name: profile.displayName,
 			token: accessToken
