5c7f09c4cb4ff169addad4ba84e6840ab2af54ee
petr0n <petron@gmail.com>
2019-07-03 15:10:09 -0500

    update more admin styles

--- client/src/components/TileList/ListItem.js
+++ client/src/components/TileList/ListItem.js
@@ -5,7 +5,7 @@ import React from "react";
 //CONTENT + EXPORT
 //=======================================================
 export const ListItem = (props) => (
-<li className="flex items-center justify-between p-3 mb-6">
+<li className="flex items-center justify-between p-3 mb-6 hover_bg-white">
     {props.children}
 </li>
 );
--- client/src/components/TilesView/TilesView.js
+++ client/src/components/TilesView/TilesView.js
@@ -5,7 +5,7 @@ import "./TilesView.css";
 // import "../List"; // componenet to hold the list
 // import "../ListItem"; // will hold each item/tile in pending tiles list
 import tileApi from "../../utils/tileAPI";
-import { List, ListItem } from "../TileList"
+import { List, ListItem } from "../TileList";
 
 // this page will act like a "page" would, where it will render the UL/LI and buttons
 //AdminNav will import his component
@@ -18,33 +18,32 @@ class TilesView extends Component {
     this.state = {
       tiles: []
       //status ?
-    }
-  };
-  
+    };
+  }
+
   // load all tiles and save them to the state
   //====================================================
   componentDidMount() {
     this.loadAllTiles();
-  };
+  }
 
   // Load all the tiles and set them to the this.state.tiles
   //====================================================
 
-
-
   loadAllTiles = () => {
-    tileApi.getTiles({ })
-    .then(res =>
-      this.setState({
-        tiles: res.data
-      })
+    tileApi
+      .getTiles({})
+      .then(res =>
+        this.setState({
+          tiles: res.data
+        })
       )
       .catch(err => console.log(err));
   };
 
   // handleEditClick
   //====================================================
-  
+
   /*
   editTile = id => {
       API.updateTile(id)
@@ -56,48 +55,52 @@ class TilesView extends Component {
 
      */
 
-
   // handleActivate/Deactivate
   //====================================================
   // taken from gameboardAPI.js file:
-// handleActivateClick =() => {
-//   updateGameTile = () => { 
-//     tileApi.updateGameTile({ })
-//     .then(res =>
-//       this.setState({
-//         tiles: res.data
-//       })
-//       )
-//       .catch(err => console.log(err));
-    
-//   };
-// }
+  // handleActivateClick =() => {
+  //   updateGameTile = () => {
+  //     tileApi.updateGameTile({ })
+  //     .then(res =>
+  //       this.setState({
+  //         tiles: res.data
+  //       })
+  //       )
+  //       .catch(err => console.log(err));
+
+  //   };
+  // }
 
   //render
   //====================================================
   render() {
     return (
-        <div>
-            {this.state.tiles.length ? (
-                <List>
-                {this.state.tiles.map(tiles => {
-                    return (
-                    <ListItem key={tiles._id}>
-                        <div id="listItem">
-                            {tiles.tileText}
-                          <button className={tiles.status==="active" ? "deactivate" : "activate"} id="toggle">{tiles.status==="active" ? "Deactivate" : "Activate"}</button>
-                          <button id="edit">Edit Me</button>
-                        </div>
-                    </ListItem>
-                    );
-                })}
-                </List>
-            ) : (
-                <h3>No tiles to display</h3>
-            )}
-        </div>
+      <div className="list">
+        {this.state.tiles.length ? (
+          <List>
+            {this.state.tiles.map(tiles => {
+              return (
+                <ListItem key={tiles._id}>
+                  <div>{tiles.tileText}</div>
+                  <div className="whitespace-no-wrap">
+                    <button
+                      className={"activate-btn rounded px-2 py-1 mr-4 toggle " + (tiles.status === "active" ? "deactivate" : "activate")}>
+                      {tiles.status === "active" ? "Deactivate" : "Activate"}
+                    </button>
+                    <button className="edit-btn rounded px-2 py-1 mr-4">
+                      Edit <i className="fas fa-pencil-alt" />
+                    </button>
+                  </div>
+                </ListItem>
+              );
+            })}
+          </List>
+        ) : (
+          <h3>No tiles to display</h3>
+        )}
+      </div>
     );
-  };
+  }
 } // ==> end TilesView
 
 //EXPORT
