5b008196b74bdfdcc80d30289301d2979d3f5501
petr0n <petron@gmail.com>
2019-07-03 20:26:52 -0500

    winner dev

--- client/src/pages/Game/Gameboard.js
+++ client/src/pages/Game/Gameboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Board from "../../components/Board";
 import Tile from "../../components/Tile";
 import gameboardAPI from '../../utils/gameboardAPI';
+import winnerAPI from '../../utils/checkForFindoAPI';
 
 
 
@@ -19,7 +20,9 @@ class Gameboard extends Component {
 			tileBigState: "inactive",
 			user: this.props.user,
 			loggedIn: this.props.loggedIn,
-			gameType: this.props.gameType
+			gameType: this.props.gameType,
+			gameboardId: "",
+			gameWon: false
 		}
 		this.handleTileBigButtonClick = this.handleTileBigButtonClick.bind(this);
 	};
@@ -37,7 +40,8 @@ class Gameboard extends Component {
 			.then(res => {
 				this.setState({ 
 					loading: false,
-					tilesData: this.renderGrid(res.data.tiles)
+					tilesData: this.renderGrid(res.data.tiles),
+					gameboardId: res.data._id
 				}) 
 			})
 			.catch(err => console.log(err))
@@ -107,7 +111,11 @@ class Gameboard extends Component {
 
 	updateGameTile = (id, isChecked) => {
 		gameboardAPI.updateGameTile(id, isChecked)
-		.then(res => console.log('tile updated', res))
+		.then(res => {
+			console.log('tile updated', res);
+			winnerAPI.checkForFindo(this.state.gameboardId)
+			.then(res => this.setState({ gameWon: res.data.winner }) );
+		})
 		.catch(err => console.log(err))
 	}
 
@@ -118,6 +126,7 @@ class Gameboard extends Component {
 		}
 		return (
 			<div className="board mx-auto">
+				<p className="text-white"> {this.state.gameWon ? "Winner" : "Loser"}</p>
 				<Board
 					tiles={this.state.tiles} 
 					tileBigState={this.state.tileBigState} 
